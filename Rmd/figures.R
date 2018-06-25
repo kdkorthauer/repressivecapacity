3c8afce24e4826be2ab6202a8589d810936fa315
kdkorthauer <kdkorthauer@gmail.com>
2018-06-25 16:40:21 -0400

    First draft of main figs

--- Rmd/figures.R
+++ Rmd/figures.R
@@ -12,12 +12,18 @@ sourceDirectory(rdir)
 
 ### FIGURE 1
 # load sub figures
-A1 <- readRDS(file="A1.rds") + ggtitle("Gene - UMR pairs") 
-A2 <- readRDS(file="A2.rds") + ggtitle("Gene - DMR pairs") 
+A1 <- readRDS(file="A1.rds") + ggtitle("Gene - UMR pairs") +
+             ylab("log2 mRNA Fold Change (+dox/no-dox)") +
+             xlab(expression(paste(Delta, "mCG in UMR (+dox - no-dox)")))
+A2 <- readRDS(file="A2.rds") + ggtitle("Gene - DMR pairs")  +
+             ylab("log2 mRNA Fold Change (+dox/no-dox)") +
+             xlab("dmrseq test statistic (+dox - no-dox)")
 B1 <- readRDS(file="B1.rds") +
-             ggtitle(expression(paste("Gene - UMR pairs (", Delta, "mCG > 0.3)")))
+             ggtitle(expression(paste("Gene - UMR pairs (", Delta, "mCG > 0.3)"))) +
+             xlab("log2 mRNA Fold Change (+dox/no-dox)")
 B2 <- readRDS(file="B2.rds") +
-             ggtitle("Gene (FDR < 0.05) - DMR pairs (FDR < 0.01)") 
+             ggtitle("Gene (FDR < 0.05) - DMR pairs (FDR < 0.01)") +
+             xlab("log2 mRNA Fold Change (+dox/no-dox)") 
 
 B1bw <- readRDS(file="B1bw.rds") +   ylab("Density") +
   ggtitle(expression(paste("Gene - UMR pairs (", Delta, "mCG > 0.3)")))
@@ -40,16 +46,18 @@ ggsave("fig1.pdf", width=8.6, height=7)
 ### FIGURE 2
 # load subfigures
 A <- readRDS(file="mCG_vs_H3K4me3_scatter_DMR.rds") + xlim(0,0.8) +
-      ylab("log2 Fold Change (dox/no dox)") +
-      ggtitle("H3K4me3 ChIP-BS")
+      ylab("log2 Fold Change (+dox/no-dox)") +
+      ggtitle("H3K4me3 ChIP-BS") +
+      xlab(expression(paste(Delta, "mCG in DMR (+dox - no-dox)")))
 B <- readRDS(file="stat_vs_H3K4me3_scatter_DMR.rds") + xlim(0,40) +
-      ylab("log2 Fold Change (dox/no dox)") +
+      ylab("log2 Fold Change (+dox/no-dox)") +
       ggtitle("H3K4me3 ChIP-BS")
 C <- readRDS(file="mCG_vs_RNApolII_scatter_DMR.rds") + xlim(0,0.8) +
-      ylab("log2 Fold Change (dox/no dox)") +
-      ggtitle("RNA PolII ChIP-BS")
+      ylab("log2 Fold Change (+dox/no-dox)") +
+      ggtitle("RNA PolII ChIP-BS") +
+      xlab(expression(paste(Delta, "mCG in DMR (+dox - no-dox)")))
 D <- readRDS(file="stat_vs_RNApolII_scatter_DMR.rds")  + xlim(0,40) +
-       ylab("log2 Fold Change (dox/no dox)") +
+       ylab("log2 Fold Change (+dox/no-dox)") +
        ggtitle("RNA PolII ChIP-BS")
 
 plot_grid(A, B, C, D,
@@ -61,9 +69,14 @@ ggsave("fig2.pdf", width=8.6, height=7)
 A <- readRDS(file="mCG_H3K4me3_boxplot_byDMR.rds")
 B <- readRDS(file="mCG_RNApolII_boxplot_byDMR.rds")
 
-plot_grid(A, B,
-          ncol = 1, labels = LETTERS[1:4])
-ggsave("fig2.pdf", width=8.6, height=7)
+leg_A <- get_legend(A + labs(color = "Treatment") + 
+                      scale_color_manual(values = c("darkblue", "darkred"),
+                                         labels = c("no-dox", "+dox")))
+
+p3 <- plot_grid(A + theme(legend.position="none"), B + theme(legend.position="none"),
+          ncol = 1, labels = LETTERS[1:2])
+plot_grid(p3, leg_A, ncol = 2, rel_widths = c(1, 0.25))
+ggsave("fig3.pdf", width=7.5, height=7)
 
 ###
 ### SUPPLEMENTARY
